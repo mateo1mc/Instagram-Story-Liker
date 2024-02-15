cb7863b87def89c87c076d05c49766b8b70a0604
MATEO C <112934949+mateo1mc@users.noreply.github.com>
2024-02-15 16:57:56 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
